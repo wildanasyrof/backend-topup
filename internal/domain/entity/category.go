9adb61fa25995f3e8f5e5160fa28634cbdea0a06
wildanasyrof <asyrof.wildan@gmail.com>
2025-09-22 13:01:15 +0700

    [ Entity ] - Category, Provider

--- internal/domain/entity/category.go
+++ internal/domain/entity/category.go
@@ -1,19 +1,37 @@
 package entity
 
 type Type string
+type CatStatus string
 
 const (
 	TypePrabayar   Type = "prabayar"
 	TypePascabayar Type = "pascabayar"
 )
 
-type Category struct {
-	ID   int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name string `json:"name" gorm:"type:varchar(255);not null;unique"`
-	Type Type   `json:"type" gorm:"type:enum('prabayar','pascabayar');not null"`
-	Menu []Menu `json:"menu,omitempty" gorm:"foreignKey:CategoryID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Slug string `json:"slug" gorm:"type:varchar(255);not null;unique"`
+const (
+	CatActive   CatStatus = "active"
+	CatInactive CatStatus = "inactive"
+	CatProblem  CatStatus = "problem"
+)
 
-	CreatedAt int64 `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt int64 `json:"updated_at" gorm:"autoUpdateTime"`
+type Category struct {
+	ID          int       `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name        string    `json:"name" gorm:"type:varchar(255);not null;unique"`
+	Type        Type      `json:"type" gorm:"type:text;not null;default=prabayar;check:cat_status_check,status IN ('prabayar','pascabayar')"`
+	MenuID      int64     `json:"menu_id" gorm:"not null;column:menu_id"`
+	ProviderID  int64     `json:"provider_id" gorm:"primaryKey;autoIncrement"`
+	Slug        string    `json:"slug" gorm:"type:varchar(255);not null;unique"`
+	Status      CatStatus `json:"status" gorm:"type:text;not null;default=inactive;check:cat_status_check,status IN ('inactive','active','problem')"`
+	Description string    `json:"description" gorm:"column:description"`
+	InputType   string    `json:"input_type" gorm:"type:varchar(255);not null"`
+	ImgUrl      string    `json:"img_url" gorm:"not null;url"`
+	IsLogin     bool      `json:"is_login"`
+	CreatedAt   int64     `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt   int64     `json:"updated_at" gorm:"autoUpdateTime"`
+
+	Menu     `json:"-" gorm:"foreignKey:MenuID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Provider `json:"-" gorm:"foreignKey:ProviderID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
+
+// TableName overrides the table name used by GORM
+func (Category) TableName() string { return "categories" }
--- internal/domain/entity/provider.go
+++ internal/domain/entity/provider.go
@@ -2,12 +2,13 @@ package entity
 
 import "time"
 
-type Menu struct {
+type Provider struct {
 	ID        int64     `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name      string    `json:"name" gorm:"size:255;not null"`
+	Slug      string    `json:"slug" gorm:"size:255;not null"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 // TableName overrides the table name used by GORM
-func (Menu) TableName() string { return "menus" }
+func (Provider) TableName() string { return "providers" }
